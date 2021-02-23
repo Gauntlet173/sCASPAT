708902975c9303b44a6ef93dc7fda232ac2e8210
Jason Morris <jmorris@smu.edu.sg>
2021-02-23 13:46:04 +0800

    removing remnant comments

--- disq_test.pl
+++ disq_test.pl
@@ -15,9 +15,3 @@ opposes(e,f,a,b).
 ?- disqualified(A,B).
 % returns 12 models, beacuse there are three disqualified rules a, c, e, and each is diqualified
 % in four ways, because each of the defeating relationships to the other two is bi-directional.
-
-% If we search for situations in which something can be disqualified but not refuted or rebutted in the normal sense:
-% 1. It opposes itself, but does not override itself.
-% 2. It opposes itself, and overrides itself.
-% 3. Two rules oppose each other, but do not override each other.
-
